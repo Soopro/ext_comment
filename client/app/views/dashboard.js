cbb8e3d8b5767a6875e1420d1e959763bfcd417c
majik <mumu296979387@gmail.com>
2015-11-20 12:10:25 +0800

    xxx

--- client/app/views/dashboard.js
+++ client/app/views/dashboard.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * @ngdoc function
  * @name commentClient.controller:DashboardCtrl
@@ -6,36 +7,38 @@
  * Controller of the commentClient
  */
 angular.module('commentClient')
-.controller('DashboardCtrl',['$cookies', function($scope, restAPI, Auth, $cookie, $location) {
-  'use strict';
+  .controller('DashboardCtrl', ['$cookies',
+    function($scope, restAPI, Auth, $cookie, $location) {
+  
 
-  $scope.comment_extension = function () {
-    var status = $cookie.get('status');
-    if (status == 0) {
-      restAPI.comment_extension.save({open_id: open_id});
-    } else if (status == 1) {
-      restAPI.comment_extension.get({open_id: open_id})
-    }
-  //   else if (status == 1) {
-  //     restAPI.comment_extension.get({open_id: open_id})
-  //     .$promise
-  //     .then(function (data) {
-  //       $scope.comment_extension = data;
-  //     })
-  //     .catch(function (data) {
-  //       console.error(data);
-  //     });
-  //   }
-  // };
+      $scope.comment_extension = function () {
+        var status = $cookie.get('status');
+        if (status == 0) {
+          restAPI.comment_extension.save({open_id: open_id});
+        } else if (status == 1) {
+          restAPI.comment_extension.get({open_id: open_id})
+        }
+      }
+      //   else if (status == 1) {
+      //     restAPI.comment_extension.get({open_id: open_id})
+      //     .$promise
+      //     .then(function (data) {
+      //       $scope.comment_extension = data;
+      //     })
+      //     .catch(function (data) {
+      //       console.error(data);
+      //     });
+      //   }
+      // };
 
-  $scope.updateCommentExtension = function() {
-    if (status == 1) {
-      $scope.comment_extension.save();
-    }
-  };
+      $scope.updateCommentExtension = function() {
+        if (status == 1) {
+          $scope.comment_extension.save();
+        }
+      };
 
-  $scope.jumpToGroups = function() {
-      $location.path('/groups');
-  };
+      $scope.jumpToGroups = function() {
+          $location.path('/groups');
+      };
 
 }]);
