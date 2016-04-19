5268dd3334d2c3cfc95be386708fb721baec0b0b
majik <mumu296979387@gmail.com>
2016-04-19 19:00:09 +0800

    xxx

--- server/deploy/testing_comment.py
+++ server/deploy/testing_comment.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 
 import multiprocessing
 
-bind = "127.0.0.1:5003"
+bind = "127.0.0.1:5001"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = "deploy/comment.access.log"
 errorlog = "deploy/comment.error.log"
